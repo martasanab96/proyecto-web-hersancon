bd36877a9e3728d3164d05182459eea42127b4de
Marta <marsanaba1@alum.us.es>
2023-10-20 21:07:23 +0200

    Carrito y productos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './images/logo.svg';
 import './assets/App.css';
-import Component from './components/Component';
+import Carrito from './components/Carrito';
+import ProductList from './components/ProductList';
+import {useState} from 'react'
+
 
 function App() {
+
+  const [allProducts, setAllProducts] = useState([]);
+	const [total, setTotal] = useState(0);
+	const [countProducts, setCountProducts] = useState(0);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Mundoo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Component />
-      </header>
+      
+
+      <Carrito
+				allProducts={allProducts}
+				setAllProducts={setAllProducts}
+				total={total}
+				setTotal={setTotal}
+				countProducts={countProducts}
+				setCountProducts={setCountProducts}
+			/>
+			<ProductList
+				allProducts={allProducts}
+				setAllProducts={setAllProducts}
+				total={total}
+				setTotal={setTotal}
+				countProducts={countProducts}
+				setCountProducts={setCountProducts}
+			/>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,33 @@
+//Info de cada producto y servicio
+//Cada producto un diccionario
+
+export const data = [
+    {
+        id: 1,
+        img: "https://lw-cdn.com/images/2B6A57CF5C96/k_db60be11cfbb895b70fd404156e5bf7e;w_957;h_957;q_70/7534341.webp",
+        nameProduct: "Bombilla LED Philips Hue",
+        price: 19.99,
+        quantity: 1
+    },
+    {
+        id: 2,
+        img: "https://www.domoticalia.es/2875-large_default/nest-espana-termostato-inteligente-de-3-generacion.jpg",
+        nameProduct: "Termostato inteligente Nest",
+        price: 199.99,
+        quantity: 1
+    },
+    {
+        id: 3,
+        img: "https://cdn.idealo.com/folder/Product/6303/6/6303639/s11_produktbild_gross/jbl-charge-4.jpg",
+        nameProduct: "Altavoz Bluetooth JBL Charge",
+        price: 129.99,
+        quantity: 1
+    },
+    {
+        id: 4,
+        img: "https://www.windowo.es/data/prod/img/ricevitore-tapparelle-smart-io-izymo-somfy-2.jpg",
+        nameProduct: "Persiana inteligente Somfy",
+        price: 149.99,
+        quantity: 1
+    }
+]
