3c2584139dbdd6d36c91b689dc9b1b3c97954eb2
albertonavas99 <albertonavas99@gmail.com>
2023-10-22 18:45:57 +0200

    Haciendo el "Contacto"

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import '../assets/Contacto.css';
+import { FaEnvelope, FaPhone, FaFax, FaMobile } from "react-icons/fa";
 
 function Contacto() {
     
     return (
-        <h1>SOBRE NOSOTROS</h1>
+        <div>
+            <h1 className='sobre-nosotros'>SOBRE NOSOTROS</h1>
+
+            <div className='col-izq'>Texto de la columna (lado izquierdo)</div>
+
+            <div className='col-der'><br /><br />
+                <FaMobile/> &nbsp; 608 925 144 / 670 791 908 <br /><br /> 
+                <FaEnvelope/> &nbsp; info@hersancon.com <br /><br />
+                <FaPhone/> &nbsp; 91 897 07 77 / 91 897 23 23 <br /> <br />
+                <FaFax/> &nbsp; 91 897 23 23
+            </div>
+
+        </div>
     )
 }
 
