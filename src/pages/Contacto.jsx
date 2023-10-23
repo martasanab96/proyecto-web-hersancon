b94714efed385e686e5162ce80054e8f093e60ac
albertonavas99 <albertonavas99@gmail.com>
2023-10-23 20:31:38 +0200

    Sobre nosotros hecho

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -8,9 +8,15 @@ function Contacto() {
         <div>
             <h1 className='sobre-nosotros'>SOBRE NOSOTROS</h1>
 
-            <div className='col-izq'>Texto de la columna (lado izquierdo)</div>
+            <div className='col-izq'>
+                Somos una empresa familiar con una amplia experiencia de más de 60 años en las distintas actividades que desempeñamos.
+                <br></br><br></br>
+                Estamos ubicados en Las Navas del Marqués, en la frontera entre las provincias de Ávila y Madrid. Nuestra área de trabajo principal abarca Madrid y Castilla y León, aunque hemos llevado a cabo proyectos en toda España.
+                <br></br><br></br>
+                Nuestra principal prioridad es brindar un excelente servicio al cliente y asegurar su satisfacción. Estamos comprometidos en ofrecer calidad y profesionalidad en cada proyecto que realizamos.
+            </div>
 
-            <div className='col-der'><br /><br />
+            <div className='col-der'>
                 <FaMobile/> &nbsp; 608 925 144 / 670 791 908 <br /><br /> 
                 <FaEnvelope/> &nbsp; info@hersancon.com <br /><br />
                 <FaPhone/> &nbsp; 91 897 07 77 / 91 897 23 23 <br /> <br />
